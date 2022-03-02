33e3453393961848c0974d28bc8e89fa936032f4
vipvenom <85557190+vipvenom@users.noreply.github.com>
2022-03-02 22:49:22 +0530

    Update pterodactyl.py

--- pterodactyl.py
+++ pterodactyl.py
@@ -1,12 +1,21 @@
-import discord, asyncio
+import discord
 from discord.ext import commands
+import aiohttp
+import random
+import requests
+import asyncio
 from pydactyl import PterodactylClient
-
 api = PterodactylClient('Your panel domain or ip', 'your user API ')
-bot = commands.Bot(command_prefix="!", case_insensitive=True)
-actions = ['start', 'stop', 'restart', 'kill']
 
-@bot.command()
+client = commands.Bot(command_prefix="!", case_insensitive=True)
+
+
+
+
+
+
+
+@client.command()
 @commands.is_owner() 
 async def panel(ctx):
   serverid = "Your Server ID"
@@ -14,23 +23,31 @@ async def panel(ctx):
   await ctx.send(embed=embed)
   embed1 = discord.Embed(title="What Power Action Do You Want To Send?",description="*start , stop , restart , kill*\n\n`cancel` To Cancel The Power-Action",color=0x00ff00)
   await ctx.send(embed=embed1)
-  
+
 
   def check(m):
           return m.channel == ctx.channel and m.author == ctx.author
-  try:
-    action = await bot.wait_for('message', check=check, timeout=30)
-  except asyncio.TimeoutError:
-    await ctx.reply(embed=discord.Embed(title='You didn\'t respond in time.'))
-    return
-  command = action.content.lower()
-  if command not in actions:
-    await ctx.reply(embed=discord.Embed(title='Choose a valid option.', description='Please choose one of the following.\n'+', '.join(actions)))
-    
 
-  api.client.servers.send_power_action(serverid, command)
-  responseEmbed = discord.Embed(title=f'{command.capitalize()}ed by {ctx.author.name}#{ctx.author.discriminator}.', description=f"Power-Action `{command}` completed.", color=0x842899)
-  await ctx.send(embed=responseEmbed)
+  action = await client.wait_for('message', check=check) 
+  if action.content == "start":
+      response = api.client.servers.send_power_action(serverid, 'start')  
+      embed3 = discord.Embed(description=f"Power-Action `start` Done",color=0x842899)
+      embed3.set_footer(text=f"started By {ctx.author}")
+  if action.content == "stop":
+      response = api.client.servers.send_power_action(serverid, 'stop')  
+      embed3 = discord.Embed(description=f"Power-Action `stop` Done",color=0x842899)
+      embed3.set_footer(text=f"Stopped By {ctx.author}")
+  if action.content == "restart":
+      response = api.client.servers.send_power_action(serverid, 'restart')  
+      embed3 = discord.Embed(description=f"Power-Action `restart` Done",color=0x842899)
+      embed3.set_footer(text=f"restarted By {ctx.author}")
+  if action.content == "kill":
+      response = api.client.servers.send_power_action(serverid, 'kill')  
+      embed3 = discord.Embed(description=f"Power-Action `kill` Done",color=0x842899)
+      embed3.set_footer(text=f"killed By {ctx.author}")
+  if action.content == "cancel":
+      embed3 = discord.Embed(description=f"Action Canceled",color=0x842899)
+  await ctx.send(embed=embed3)
 
   print(f'{ctx.author.name} Used The Command panelweb')
 
@@ -40,8 +57,8 @@ async def panel(ctx):
     if isinstance(error, commands.NotOwner):
       embed = discord.Embed(title='⚠️ Restricted',description='Only Bot Owner Can Execute This Command')
       await ctx.send(embed=embed)
-    
 
 
 
-bot.run('bot Token')
+
+client.run('bot Token')
